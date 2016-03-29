59f0f53bd0736d36feb8bb3025a1f7a982ad59c3
Markus Frosch <lazyfrosch@icinga.org>
2016-03-29 16:27:10 +0200

    Use chgrp and chmod to setup config permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,12 @@ config=/etc/icingaweb2
 if [ ! -e "$config"/config.ini ]; then
     echo "Setting setup token from ICINGAWEB_SETUP_TOKEN"
     echo "$ICINGAWEB_SETUP_TOKEN" > "$config"/setup.token
-
-    echo "chowning $config"
-    # TODO: check for virtualbox based docker environments
-    chown -R www-data:www-data "$config"
 fi
+
+# TODO: check for virtualbox based docker environments
+echo "fixing permissions in $config"
+chgrp -R www-data "$config"
+find "$config" -type d -exec chmod g+ws {} \;
+find "$config" -type f -exec chmod g+w {} \;
 
 exec "$@"
