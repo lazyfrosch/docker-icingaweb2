c7c7091aafa409d6445920ab9220facd95d24c16
Markus Frosch <lazyfrosch@icinga.org>
2018-03-08 13:53:30 +0100

    Support ICINGAWEB_TIMEZONE environment variable

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 CONFIG_GROUP=${CONFIG_GROUP:-65534} # nobody on alpine
 ICINGAWEB_CONFIGDIR=${ICINGAWEB_CONFIGDIR:-'/etc/icingaweb2'}
 ICINGAWEB_SETUP_TOKEN=${ICINGAWEB_SETUP_TOKEN:-'docker'}
+ICINGAWEB_TIMEZONE=${ICINGAWEB_TIMEZONE='UTC'}
 XDEBUG_ENABLED=${XDEBUG_ENABLED:-''}
 
 export ICINGAWEB_CONFIGDIR
@@ -12,6 +13,10 @@ export ICINGAWEB_CONFIGDIR
 if [ ! -e "$ICINGAWEB_CONFIGDIR"/config.ini ]; then
   echo "Setting setup token from ICINGAWEB_SETUP_TOKEN"
   echo "$ICINGAWEB_SETUP_TOKEN" > "$ICINGAWEB_CONFIGDIR"/setup.token
+fi
+
+if [ -n "$ICINGAWEB_TIMEZONE" ]; then
+    echo "date.timezone = $ICINGAWEB_TIMEZONE" > etc/php7/conf.d/timezone.ini
 fi
 
 if [ `stat -c %g "$ICINGAWEB_CONFIGDIR"` != "$CONFIG_GROUP" ]; then
